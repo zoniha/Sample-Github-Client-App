ebc537c62b411965546ddf85ffa4b2d975a84dac
zoniha <awaawapurin@gmail.com>
2021-08-13 22:45:17 +0900

    Initial Commit

---
+++ SampleGithubClientApp/SampleGithubClientAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleGithubClientAppApp.swift
+//  SampleGithubClientApp
+//
+//  Created by zoniha on 2021/08/13.
+//
+
+import SwiftUI
+
+@main
+struct SampleGithubClientAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
