f193d9edeef7d0e01c5523d54f87b4c14ef03390
zoniha <awaawapurin@gmail.com>
2021-08-14 23:28:14 +0900

    🎉 init: Initial commit

---
+++ SampleGithubClientApp/Views/RepoDetailView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct RepoDetailView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct RepoDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        RepoDetailView()
+    }
+}
