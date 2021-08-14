3ce14616987f9c8dd0be49fb20338ff861b85b57
zoniha <awaawapurin@gmail.com>
2021-08-14 23:52:33 +0900

    🎉 init: Create mock data

---
+++ SampleGithubClientApp/Preview Content/Mocks/Repo+mock.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension Repo {
+	static let mock1 = Repo(id: 1, name: "Test Repo1", owner: .mock1, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", stargazersCount: 10)
+	static let mock2 = Repo(id: 2, name: "Test Repo2", owner: .mock2, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", stargazersCount: 10)
+	static let mock3 = Repo(id: 3, name: "Test Repo3", owner: .mock3, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", stargazersCount: 10)
+	static let mock4 = Repo(id: 4, name: "Test Repo4", owner: .mock4, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", stargazersCount: 10)
+	static let mock5 = Repo(id: 5, name: "Test Repo5", owner: .mock5, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", stargazersCount: 10)
+	static let mock6 = Repo(id: 6, name: "Test Repo6", owner: .mock6, description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", stargazersCount: 10)
+
+}
---
+++ SampleGithubClientApp/Preview Content/Mocks/User+mock.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+extension User {
+	static let mock1 = User(name: "User1")
+	static let mock2 = User(name: "User2")
+	static let mock3 = User(name: "User3")
+	static let mock4 = User(name: "User4")
+	static let mock5 = User(name: "User5")
+	static let mock6 = User(name: "User6")
+}
