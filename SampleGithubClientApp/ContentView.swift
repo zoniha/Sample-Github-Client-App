9e7c0f81f9a9af0430251618a67c6c07253a2d32
zoniha <awaawapurin@gmail.com>
2021-08-14 11:52:32 +0900

    ✨ feat: Update main page layout

--- SampleGithubClientApp/ContentView.swift
+++ SampleGithubClientApp/ContentView.swift
@@ -1,21 +1,24 @@
-//
-//  ContentView.swift
-//  SampleGithubClientApp
-//
-//  Created by zoniha on 2021/08/13.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+	var body: some View {
+		HStack {
+			Image("GitHubMark")
+				.resizable()
+				.frame(width: 44, height: 44, alignment: .center)
+			VStack(alignment: .leading) {
+				Text("Owner Name")
+					.font(.caption)
+				Text("Repository Name")
+					.fontWeight(.semibold)
+					.font(.body)
+			}
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
