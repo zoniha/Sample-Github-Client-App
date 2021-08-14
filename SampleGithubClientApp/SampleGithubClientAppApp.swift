bf14b956cc68e41bd8e0f394c4ddcd3e901259c4
zoniha <awaawapurin@gmail.com>
2021-08-14 23:27:38 +0900

    ☪️ refactor: Rename

--- SampleGithubClientApp/SampleGithubClientAppApp.swift
+++ SampleGithubClientApp/SampleGithubClientAppApp.swift
@@ -1,17 +1,10 @@
-//
-//  SampleGithubClientAppApp.swift
-//  SampleGithubClientApp
-//
-//  Created by zoniha on 2021/08/13.
-//
-
 import SwiftUI
 
 @main
 struct SampleGithubClientAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RepoListView()
         }
     }
 }
