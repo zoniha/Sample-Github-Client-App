d18440e0f8278fd2b511f9aef40c365d1d5adad1
zoniha <awaawapurin@gmail.com>
2021-08-14 21:54:35 +0900

    ☪️ refactor: Separate repo list view from content view

--- SampleGithubClientApp/ContentView.swift
+++ SampleGithubClientApp/ContentView.swift
@@ -1,18 +1,18 @@
 import SwiftUI
 
 struct ContentView: View {
+	private let mockRepos = [
+		Repo(id: 1, name: "Test Repo1", owner: User(name: "User1")),
+		Repo(id: 2, name: "Test Repo2", owner: User(name: "User2")),
+		Repo(id: 3, name: "Test Repo3", owner: User(name: "User3")),
+		Repo(id: 4, name: "Test Repo4", owner: User(name: "User4")),
+		Repo(id: 5, name: "Test Repo5", owner: User(name: "User5")),
+		Repo(id: 6, name: "Test Repo6", owner: User(name: "User6")),
+	]
+
 	var body: some View {
-		List(0 ..< 4) { item in
-			Image("GitHubMark")
-				.resizable()
-				.frame(width: 44, height: 44, alignment: .center)
-			VStack(alignment: .leading) {
-				Text("Owner Name")
-					.font(.caption)
-				Text("Repository Name")
-					.fontWeight(.semibold)
-					.font(.body)
-			}
+		List(mockRepos) { repo in
+			RepoRow(repo: repo)
 		}
 	}
 }
---
+++ SampleGithubClientApp/RepoRow.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+struct RepoRow: View {
+	let repo: Repo
+
+	var body: some View {
+		HStack {
+			Image("GitHubMark")
+				.resizable()
+				.frame(width: 44, height: 44, alignment: .center)
+			VStack(alignment: .leading) {
+				Text(repo.owner.name)
+					.font(.caption)
+				Text(repo.name)
+					.fontWeight(.semibold)
+					.font(.body)
+			}
+		}
+	}
+}
+
+struct RepoRow_Previews: PreviewProvider {
+	static var previews: some View {
+		RepoRow(repo: Repo(id: 1, name: "Test Repo1", owner: User(name: "User1")))
+	}
+}
