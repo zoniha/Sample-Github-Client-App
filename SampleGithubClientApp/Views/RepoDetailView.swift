93f9b073014fc22ef507ef4a5d03e58595436063
zoniha <awaawapurin@gmail.com>
2021-08-15 12:00:21 +0900

    ✨ feat: Create layout

--- SampleGithubClientApp/Views/RepoDetailView.swift
+++ SampleGithubClientApp/Views/RepoDetailView.swift
@@ -4,7 +4,29 @@ struct RepoDetailView: View {
 	let repo: Repo
 	
 	var body: some View {
-		Text("")
+		HStack {
+			VStack(alignment: .leading) {
+				HStack {
+					Image("GitHubMark")
+						.resizable()
+						.frame(width: 16, height: 16)
+					Text(repo.owner.name)
+						.font(.caption)
+				}
+				Text(repo.name)
+					.font(.body)
+					.fontWeight(.semibold)
+				Text(repo.description)
+					.padding(.top, 8)
+				HStack {
+					Image(systemName: "star")
+					Text("\(repo.stargazersCount) stars")
+				}
+				Spacer()
+			}
+			.padding()
+			Spacer()
+		}
 	}
 }
 
